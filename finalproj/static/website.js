11e0ba9a6b46d10dd09f63cc39d6771954586298
Matt-Swaim <112338742+Matt-Swaim@users.noreply.github.com>
2024-04-22 00:33:27 -0400

    initial website.js

--- finalproj/static/website.js
+++ finalproj/static/website.js
@@ -1,1 +1,20 @@
+function display(textString, onSuccess) {
+    $.ajax({
+	url: "string_return",
+	data: {
+	    text_string: textString
+        },
+	dataType: "json",
+        type: "GET",
+	success: function(response) {
+	    onSuccess(response);
+	}
+    });
+}
 
+$("#convert-button").on("click", function() {
+    let textString = $("#text-input").val();
+    display(textString, function(returnedString) {
+	$("#returned_string").text(returnedString);
+    });
+});
