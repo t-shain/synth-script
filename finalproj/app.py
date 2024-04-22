a717fa37c00303fc76debe26236910a79b80318b
Matt-Swaim <112338742+Matt-Swaim@users.noreply.github.com>
2024-04-22 00:45:54 -0400

    Add files via upload

--- finalproj/app.py
+++ finalproj/app.py
@@ -3,16 +3,12 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/led_on", methods=["POST"])
-def led_on_r():
-    print("LED on")
-    return "ok"
-
-@app.route("/led_off", methods=["POST"])
-def led_off_r():
-    print("LED off")
-    return "ok"
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("website.html")
+
+@app.route("/string_return", methods=["GET"])
+def string_return():
+    text_string = request.values["text_string"]
+    return json.dumps(text_string)
